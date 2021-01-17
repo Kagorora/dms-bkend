3baa4b237079dd5233866dd62658148db66ce3bf
Kagorora <kagororamaxime@gmail.com>
2021-01-17 17:25:29 +0200

    cleanup by adding controller and scr folder

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./src/config/db.js";
+import productRoutes from "./src/routes/productRoutes.js";
+import { notFound, errorHandler } from "./src/middlewares/errorMiddleware.js";
 
 const app = express();
 dotenv.config();

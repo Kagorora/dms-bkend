3a04566f50ad5adc0df274d64dfde5bf9d3c44d9
Kagorora <kagororamaxime@gmail.com>
2021-02-08 22:27:08 +0200

    api to update paid to true

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { addOrderItem, getOrderById } from "../controllers/orderController.js";
+import {
+  addOrderItem,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 const routes = express.Router();
 
 routes.route("/").post(protect, addOrderItem);
 routes.route("/:id").get(protect, getOrderById);
+routes.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default routes;
